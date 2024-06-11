08ded4dc5ac3e03520dc3ab7f25b0396b6d4c504
azzizulhakim810 <azzizul.hakim202@gmail.com>
2024-06-11 18:32:26 +0600

    Upload the frontend & backend

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DisplayData from "./components/DisplayData/DisplayData";
+
+function App() {
+  return (
+    <>
+      <div>
+        <DisplayData />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Client/src/components/DisplayData/DisplayData.jsx
@@ -0,0 +1,33 @@
+// Import everything needed to use the `useQuery` hook
+import { gql, useQuery } from "@apollo/client";
+
+const GET_ALL_BOOKS = gql`
+  query getBooks {
+    getBooks {
+      _id
+      name
+      author
+      price
+    }
+  }
+`;
+
+const DisplayData = () => {
+  const { loading, error, data } = useQuery(GET_ALL_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  // if (data) return console.log(data.getBooks);
+  return data?.getBooks?.map(({ _id, name, author, price }) => (
+    <div key={_id}>
+      <h3>{name}</h3>
+      <br />
+      <b>About this Book:</b>
+      <p>{author}</p>
+      <p>{price}</p>
+      <br />
+    </div>
+  ));
+};
+
+export default DisplayData;
